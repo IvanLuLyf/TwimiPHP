5fc5c5aaca369b19cacddefd0332f978f86da60c
IvanLuLyf <835498692@qq.com>
2018-09-24 00:33:50 +0800

    Add Rewrite Setting

--- api.php
+++ api.php
@@ -1,7 +1,8 @@
 <?php
-header("Content-Type: text/html; charset=UTF-8");
+header("Content-Type: application/json; charset=UTF-8");
 define('APP_PATH', __DIR__ . '/');
 define("IN_TWIMI_PHP", "True", TRUE);
 define("IN_TWIMI_API", "True", TRUE);
 include "config/config.php";
 include "core/core.php";
+tp_run();
